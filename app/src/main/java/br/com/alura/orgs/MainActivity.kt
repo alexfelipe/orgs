cb89d5be6b17d68cfa861883db545157eeab2cf5
Alex Felipe <alexfelipevieira@gmail.com>
2021-04-14 13:06:35 -0300

    1.5

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -1,6 +1,14 @@
 package br.com.alura.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Alex Felipe", Toast.LENGTH_SHORT).show()
+    }
+
 }
