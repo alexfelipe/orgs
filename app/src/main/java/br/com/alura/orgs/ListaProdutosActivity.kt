308785bdedd80f7142b84fe362f5c14e34274d2a
Alex Felipe <alexfelipevieira@gmail.com>
2021-04-19 09:10:41 -0300

    3.3

--- app/src/main/java/br/com/alura/orgs/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ListaProdutosActivity.kt
@@ -2,19 +2,31 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.databinding.ActivityListaProdutosBinding
 import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+private const val TAG = "ListaProdutosActivity"
+
+class ListaProdutosActivity : Activity() {
+
+    private val binding by lazy {
+        ActivityListaProdutosBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+        setContentView(binding.root)
+        configuraRecyclerView()
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerview = binding.recyclerview
         recyclerview.adapter = ListaProdutosAdapter(
             this,
             listOf(
--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,12 +1,14 @@
 package br.com.alura.orgs.ui.recyclerview.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.databinding.ProdutoItemBinding
 import br.com.alura.orgs.model.Produto
 
 class ListaProdutosAdapter(
@@ -14,23 +16,21 @@ class ListaProdutosAdapter(
     private val produtos: List<Produto>
 ) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val binding: ProdutoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun vincula(produto: Produto) {
-            val nome = itemView.findViewById<TextView>(R.id.nome)
-            nome.text = produto.nome
-            val descricao = itemView.findViewById<TextView>(R.id.descricao)
-            descricao.text = produto.descricao
-            val valor = itemView.findViewById<TextView>(R.id.valor)
-            valor.text = produto.valor.toPlainString()
+            binding.nome.text = produto.nome
+            binding.descricao.text = produto.descricao
+            binding.valor.text = produto.valor.toPlainString()
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
-        val viewCriada = inflater.inflate(R.layout.produto_item, parent, false)
-        return ViewHolder(viewCriada)
+        val binding = ProdutoItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
